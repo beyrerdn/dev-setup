fce7e0f2b002e53ef78fd03fc605d96a1af74f67
Danny Beyrer <beyrerdn@gmail.com>
2021-03-21 22:13:08 -0400

    update datastores

--- datastores.sh
+++ datastores.sh
@@ -24,11 +24,15 @@ brew install postgresql
 brew install mongo
 brew install redis
 brew install elasticsearch
+brew install graphql-cli
 
 # Install mysql workbench
+# Install graphiql https://www.electronjs.org/apps/graphiql
 # Install Cask
 brew install caskroom/cask/brew-cask
-brew cask install --appdir="/Applications" mysqlworkbench
+brew install --cask --appdir="/Applications" mysqlworkbench
+brew install --cask --appdir="/Applications" graphiql
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
